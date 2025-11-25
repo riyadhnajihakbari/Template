023d5fccdfb666df47680307068e7dde196e993d
Riyadh Najih <riyadhnajih@gmail.com>
2025-11-25 22:10:08 +0700

    2

--- sportifyx/app/Http/Controllers/EventController.php
+++ sportifyx/app/Http/Controllers/EventController.php
@@ -8,41 +8,130 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    /**
+     * Data provinsi dan kota di Indonesia
+     */
+    private function getProvinces()
+    {
+        return [
+            'aceh' => 'Aceh',
+            'sumut' => 'Sumatera Utara',
+            'sumbar' => 'Sumatera Barat',
+            'riau' => 'Riau',
+            'kepri' => 'Kepulauan Riau',
+            'jambi' => 'Jambi',
+            'sumsel' => 'Sumatera Selatan',
+            'babel' => 'Bangka Belitung',
+            'bengkulu' => 'Bengkulu',
+            'lampung' => 'Lampung',
+            'jakarta' => 'DKI Jakarta',
+            'jabar' => 'Jawa Barat',
+            'banten' => 'Banten',
+            'jateng' => 'Jawa Tengah',
+            'jogja' => 'DI Yogyakarta',
+            'jatim' => 'Jawa Timur',
+            'bali' => 'Bali',
+            'ntb' => 'Nusa Tenggara Barat',
+            'ntt' => 'Nusa Tenggara Timur',
+            'kalbar' => 'Kalimantan Barat',
+            'kalteng' => 'Kalimantan Tengah',
+            'kalsel' => 'Kalimantan Selatan',
+            'kaltim' => 'Kalimantan Timur',
+            'kaltara' => 'Kalimantan Utara',
+            'sulut' => 'Sulawesi Utara',
+            'sulteng' => 'Sulawesi Tengah',
+            'sulsel' => 'Sulawesi Selatan',
+            'sultra' => 'Sulawesi Tenggara',
+            'gorontalo' => 'Gorontalo',
+            'sulbar' => 'Sulawesi Barat',
+            'maluku' => 'Maluku',
+            'malut' => 'Maluku Utara',
+            'papbar' => 'Papua Barat',
+            'papua' => 'Papua',
+        ];
+    }
+
+    private function getCitiesByProvince()
+    {
+        return [
+            'aceh' => ['Banda Aceh', 'Aceh Besar', 'Aceh Barat', 'Aceh Selatan', 'Aceh Timur', 'Sabang', 'Lhokseumawe', 'Langsa'],
+            'sumut' => ['Medan', 'Binjai', 'Tebing Tinggi', 'Pematang Siantar', 'Tanjung Balai', 'Deli Serdang', 'Karo', 'Langkat'],
+            'sumbar' => ['Padang', 'Bukittinggi', 'Payakumbuh', 'Padang Panjang', 'Solok', 'Sawahlunto', 'Pariaman'],
+            'riau' => ['Pekanbaru', 'Dumai', 'Bengkalis', 'Kampar', 'Rokan Hulu', 'Siak', 'Pelalawan'],
+            'kepri' => ['Batam', 'Tanjung Pinang', 'Bintan', 'Karimun', 'Natuna', 'Lingga'],
+            'jambi' => ['Jambi', 'Sungai Penuh', 'Batang Hari', 'Bungo', 'Kerinci', 'Merangin', 'Muaro Jambi'],
+            'sumsel' => ['Palembang', 'Prabumulih', 'Pagar Alam', 'Lubuk Linggau', 'Banyuasin', 'Musi Banyuasin', 'Ogan Ilir'],
+            'babel' => ['Pangkal Pinang', 'Bangka', 'Belitung', 'Bangka Barat', 'Bangka Tengah', 'Bangka Selatan'],
+            'bengkulu' => ['Bengkulu', 'Bengkulu Utara', 'Bengkulu Selatan', 'Bengkulu Tengah', 'Kaur', 'Rejang Lebong'],
+            'lampung' => ['Bandar Lampung', 'Metro', 'Lampung Selatan', 'Lampung Tengah', 'Lampung Utara', 'Lampung Timur'],
+            'jakarta' => ['Jakarta Pusat', 'Jakarta Utara', 'Jakarta Barat', 'Jakarta Selatan', 'Jakarta Timur', 'Kepulauan Seribu'],
+            'jabar' => ['Bandung', 'Bekasi', 'Bogor', 'Cirebon', 'Depok', 'Sukabumi', 'Tasikmalaya', 'Cimahi', 'Banjar', 'Bandung Barat', 'Garut', 'Indramayu', 'Karawang', 'Kuningan', 'Majalengka', 'Pangandaran', 'Purwakarta', 'Subang', 'Sumedang'],
+            'banten' => ['Serang', 'Tangerang', 'Cilegon', 'Tangerang Selatan', 'Pandeglang', 'Lebak', 'Tangerang (Kab.)'],
+            'jateng' => ['Semarang', 'Surakarta', 'Magelang', 'Salatiga', 'Pekalongan', 'Tegal', 'Banyumas', 'Cilacap', 'Kebumen', 'Purworejo', 'Wonosobo', 'Boyolali', 'Klaten', 'Sukoharjo', 'Wonogiri', 'Karanganyar', 'Sragen', 'Blora', 'Grobogan', 'Kudus', 'Jepara', 'Demak', 'Semarang (Kab.)', 'Temanggung', 'Kendal', 'Batang', 'Pekalongan (Kab.)', 'Pemalang', 'Tegal (Kab.)', 'Brebes', 'Magelang (Kab.)', 'Purbalingga', 'Banjarnegara'],
+            'jogja' => ['Yogyakarta', 'Sleman', 'Bantul', 'Kulon Progo', 'Gunungkidul'],
+            'jatim' => ['Surabaya', 'Malang', 'Batu', 'Blitar', 'Kediri', 'Madiun', 'Mojokerto', 'Pasuruan', 'Probolinggo', 'Gresik', 'Sidoarjo', 'Bangkalan', 'Banyuwangi', 'Bondowoso', 'Jember', 'Lumajang', 'Situbondo', 'Bojonegoro', 'Lamongan', 'Tuban', 'Jombang', 'Nganjuk', 'Ponorogo', 'Tulungagung', 'Trenggalek', 'Pacitan', 'Magetan', 'Ngawi'],
+            'bali' => ['Denpasar', 'Badung', 'Gianyar', 'Tabanan', 'Klungkung', 'Bangli', 'Karangasem', 'Buleleng', 'Jembrana'],
+            'ntb' => ['Mataram', 'Bima', 'Lombok Barat', 'Lombok Tengah', 'Lombok Timur', 'Lombok Utara', 'Sumbawa', 'Dompu'],
+            'ntt' => ['Kupang', 'Ende', 'Alor', 'Belu', 'Flores Timur', 'Lembata', 'Manggarai', 'Ngada', 'Sikka', 'Sumba Barat', 'Sumba Timur', 'Timor Tengah Selatan'],
+            'kalbar' => ['Pontianak', 'Singkawang', 'Bengkayang', 'Kapuas Hulu', 'Kayong Utara', 'Ketapang', 'Kubu Raya', 'Landak', 'Melawi', 'Sambas', 'Sanggau', 'Sekadau', 'Sintang'],
+            'kalteng' => ['Palangkaraya', 'Barito Selatan', 'Barito Timur', 'Barito Utara', 'Gunung Mas', 'Kapuas', 'Katingan', 'Kotawaringin Barat', 'Kotawaringin Timur', 'Lamandau', 'Murung Raya', 'Pulang Pisau', 'Seruyan', 'Sukamara'],
+            'kalsel' => ['Banjarmasin', 'Banjarbaru', 'Banjar', 'Barito Kuala', 'Hulu Sungai Selatan', 'Hulu Sungai Tengah', 'Hulu Sungai Utara', 'Kotabaru', 'Tabalong', 'Tanah Bumbu', 'Tanah Laut', 'Tapin', 'Balangan'],
+            'kaltim' => ['Samarinda', 'Balikpapan', 'Bontang', 'Berau', 'Kutai Barat', 'Kutai Kartanegara', 'Kutai Timur', 'Mahakam Ulu', 'Paser', 'Penajam Paser Utara'],
+            'kaltara' => ['Tarakan', 'Bulungan', 'Malinau', 'Nunukan', 'Tana Tidung'],
+            'sulut' => ['Manado', 'Bitung', 'Tomohon', 'Kotamobagu', 'Bolaang Mongondow', 'Minahasa', 'Minahasa Selatan', 'Minahasa Tenggara', 'Minahasa Utara', 'Kepulauan Sangihe', 'Kepulauan Talaud'],
+            'sulteng' => ['Palu', 'Banggai', 'Banggai Kepulauan', 'Buol', 'Donggala', 'Morowali', 'Parigi Moutong', 'Poso', 'Sigi', 'Tojo Una-Una', 'Tolitoli'],
+            'sulsel' => ['Makassar', 'Palopo', 'Parepare', 'Bantaeng', 'Barru', 'Bone', 'Bulukumba', 'Enrekang', 'Gowa', 'Jeneponto', 'Kepulauan Selayar', 'Luwu', 'Luwu Timur', 'Luwu Utara', 'Maros', 'Pangkajene Kepulauan', 'Pinrang', 'Sidenreng Rappang', 'Sinjai', 'Soppeng', 'Takalar', 'Tana Toraja', 'Toraja Utara', 'Wajo'],
+            'sultra' => ['Kendari', 'Bau-Bau', 'Bombana', 'Buton', 'Kolaka', 'Konawe', 'Konawe Selatan', 'Konawe Utara', 'Muna', 'Wakatobi'],
+            'gorontalo' => ['Gorontalo', 'Boalemo', 'Bone Bolango', 'Gorontalo Utara', 'Pohuwato'],
+            'sulbar' => ['Mamuju', 'Majene', 'Mamasa', 'Mamuju Utara', 'Polewali Mandar'],
+            'maluku' => ['Ambon', 'Tual', 'Buru', 'Buru Selatan', 'Kepulauan Aru', 'Maluku Tengah', 'Maluku Tenggara', 'Maluku Tenggara Barat', 'Seram Bagian Barat', 'Seram Bagian Timur'],
+            'malut' => ['Ternate', 'Tidore Kepulauan', 'Halmahera Barat', 'Halmahera Tengah', 'Halmahera Timur', 'Halmahera Selatan', 'Halmahera Utara', 'Kepulauan Sula', 'Pulau Morotai'],
+            'papbar' => ['Manokwari', 'Sorong', 'Fakfak', 'Kaimana', 'Manokwari Selatan', 'Maybrat', 'Pegunungan Arfak', 'Raja Ampat', 'Sorong Selatan', 'Tambrauw', 'Teluk Bintuni', 'Teluk Wondama'],
+            'papua' => ['Jayapura', 'Asmat', 'Biak Numfor', 'Boven Digoel', 'Deiyai', 'Dogiyai', 'Intan Jaya', 'Jayawijaya', 'Keerom', 'Kepulauan Yapen', 'Lanny Jaya', 'Mamberamo Raya', 'Mamberamo Tengah', 'Mappi', 'Merauke', 'Mimika', 'Nabire', 'Nduga', 'Paniai', 'Pegunungan Bintang', 'Puncak', 'Puncak Jaya', 'Sarmi', 'Supiori', 'Tolikara', 'Waropen', 'Yahukimo', 'Yalimo'],
+        ];
+    }
+
     public function index(Request $request)
     {
         $query = Event::with(['sport', 'tickets'])
-            ->where('status', 'published')
-            ->where('tanggal_mulai', '>', now());
+            ->where('tanggal_mulai', '>=', now())
+            ->orderBy('tanggal_mulai', 'asc');
 
-        if ($request->sport) {
-            $query->whereHas('sport', function ($q) use ($request) {
+        // Filter by sport
+        if ($request->filled('sport')) {
+            $query->whereHas('sport', function($q) use ($request) {
                 $q->where('slug', $request->sport);
             });
         }
 
-        if ($request->date) {
+        // Filter by date
+        if ($request->filled('date')) {
             $query->whereDate('tanggal_mulai', $request->date);
         }
 
-        if ($request->location) {
-            $query->where('lokasi', 'like', '%' . $request->location . '%');
+        // Filter by province
+        if ($request->filled('province')) {
+            $provinceName = $this->getProvinces()[$request->province] ?? '';
+            $query->where('lokasi', 'LIKE', "%{$provinceName}%");
         }
 
-        if ($request->min_price || $request->max_price) {
-            $query->whereHas('tickets', function ($q) use ($request) {
-                if ($request->min_price) {
-                    $q->where('harga', '>=', $request->min_price);
-                }
-                if ($request->max_price) {
-                    $q->where('harga', '<=', $request->max_price);
-                }
-            });
+        // Filter by city
+        if ($request->filled('city')) {
+            $query->where('lokasi', 'LIKE', "%{$request->city}%");
         }
 
-        $events = $query->orderBy('tanggal_mulai')->paginate(12);
+        $events = $query->paginate(12);
         $sports = Sport::all();
+        $provinces = $this->getProvinces();
+        $allCities = $this->getCitiesByProvince();
+        
+        // Get cities for selected province
+        $cities = [];
+        if ($request->filled('province')) {
+            $cities = $allCities[$request->province] ?? [];
+        }
 
-        return view('events.index', compact('events', 'sports'));
+        return view('events.index', compact('events', 'sports', 'provinces', 'cities', 'allCities'));
     }
 
     public function show(Event $event)
@@ -51,15 +140,31 @@ class EventController extends Controller
         return view('events.show', compact('event'));
     }
 
-    public function bySport(Sport $sport)
+    public function bySport($slug)
     {
-        $events = Event::with('tickets')
+        $sport = Sport::where('slug', $slug)->firstOrFail();
+        
+        $events = Event::with(['sport', 'tickets'])
             ->where('sport_id', $sport->id)
-            ->where('status', 'published')
-            ->where('tanggal_mulai', '>', now())
-            ->orderBy('tanggal_mulai')
+            ->where('tanggal_mulai', '>=', now())
+            ->orderBy('tanggal_mulai', 'asc')
             ->paginate(12);
 
-        return view('events.by-sport', compact('events', 'sport'));
+        $sports = Sport::all();
+        $provinces = $this->getProvinces();
+        $allCities = $this->getCitiesByProvince();
+
+        return view('events.index', compact('events', 'sports', 'sport', 'provinces', 'allCities'));
+    }
+
+    /**
+     * API endpoint untuk mendapatkan kota berdasarkan provinsi (optional, untuk AJAX)
+     */
+    public function getCities(Request $request)
+    {
+        $province = $request->get('province');
+        $cities = $this->getCitiesByProvince()[$province] ?? [];
+        
+        return response()->json($cities);
     }
 }
--- sportifyx/resources/views/events/index.blade.php
+++ sportifyx/resources/views/events/index.blade.php
@@ -8,73 +8,228 @@
 
     <!-- Filters -->
     <div class="bg-white rounded-lg shadow-md p-6 mb-8">
-        <form method="GET" action="{{ route('events.index') }}" class="grid md:grid-cols-4 gap-4">
+        <form method="GET" action="{{ route('events.index') }}" id="filterForm" class="grid md:grid-cols-5 gap-4">
             <div>
                 <label class="block text-sm font-medium mb-1">Kategori Olahraga</label>
-                <select name="sport" class="w-full border rounded px-3 py-2">
-                    <option value="">Semua</option>
+                <select name="sport" class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                    <option value="">Semua Kategori</option>
                     @foreach($sports as $sport)
                         <option value="{{ $sport->slug }}" {{ request('sport') == $sport->slug ? 'selected' : '' }}>
-                            {{ $sport->name }}
+                            {{ $sport->icon }} {{ $sport->name }}
                         </option>
                     @endforeach
                 </select>
             </div>
+            
             <div>
                 <label class="block text-sm font-medium mb-1">Tanggal</label>
-                <input type="date" name="date" value="{{ request('date') }}" class="w-full border rounded px-3 py-2">
+                <input type="date" name="date" value="{{ request('date') }}" class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent">
             </div>
+            
             <div>
-                <label class="block text-sm font-medium mb-1">Lokasi</label>
-                <input type="text" name="location" value="{{ request('location') }}" placeholder="Cari lokasi..." class="w-full border rounded px-3 py-2">
+                <label class="block text-sm font-medium mb-1">Provinsi</label>
+                <select name="province" id="provinceSelect" class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                    <option value="">Semua Provinsi</option>
+                    @foreach($provinces as $code => $name)
+                        <option value="{{ $code }}" {{ request('province') == $code ? 'selected' : '' }}>
+                            {{ $name }}
+                        </option>
+                    @endforeach
+                </select>
             </div>
-            <div class="flex items-end">
-                <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition">
-                    Filter
+            
+            <div>
+                <label class="block text-sm font-medium mb-1">Kota/Kabupaten</label>
+                <select name="city" id="citySelect" class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent" {{ !request('province') ? 'disabled' : '' }}>
+                    <option value="">Pilih Provinsi Dulu</option>
+                    @if(request('province') && isset($cities))
+                        @foreach($cities as $city)
+                            <option value="{{ $city }}" {{ request('city') == $city ? 'selected' : '' }}>
+                                {{ $city }}
+                            </option>
+                        @endforeach
+                    @endif
+                </select>
+            </div>
+            
+            <div class="flex items-end gap-2">
+                <button type="submit" class="flex-1 bg-gradient-to-r from-blue-600 to-purple-600 text-white py-2 rounded-lg hover:from-blue-700 hover:to-purple-700 transition font-medium">
+                    🔍 Filter
                 </button>
+                <a href="{{ route('events.index') }}" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition">
+                    ✕
+                </a>
             </div>
         </form>
+    </div>
+
+    <!-- Active Filters Display -->
+    @if(request()->hasAny(['sport', 'date', 'province', 'city']))
+        <div class="mb-6 flex flex-wrap gap-2">
+            <span class="text-sm text-gray-600">Filter aktif:</span>
+            
+            @if(request('sport'))
+                <span class="inline-flex items-center px-3 py-1 rounded-full text-sm bg-blue-100 text-blue-800">
+                    Olahraga: {{ collect($sports)->where('slug', request('sport'))->first()->name ?? request('sport') }}
+                    <a href="{{ route('events.index', array_diff_key(request()->all(), ['sport' => ''])) }}" class="ml-2 text-blue-600 hover:text-blue-800">×</a>
+                </span>
+            @endif
+            
+            @if(request('date'))
+                <span class="inline-flex items-center px-3 py-1 rounded-full text-sm bg-green-100 text-green-800">
+                    Tanggal: {{ \Carbon\Carbon::parse(request('date'))->format('d M Y') }}
+                    <a href="{{ route('events.index', array_diff_key(request()->all(), ['date' => ''])) }}" class="ml-2 text-green-600 hover:text-green-800">×</a>
+                </span>
+            @endif
+            
+            @if(request('province'))
+                <span class="inline-flex items-center px-3 py-1 rounded-full text-sm bg-purple-100 text-purple-800">
+                    Provinsi: {{ $provinces[request('province')] ?? request('province') }}
+                    <a href="{{ route('events.index', array_diff_key(request()->all(), ['province' => '', 'city' => ''])) }}" class="ml-2 text-purple-600 hover:text-purple-800">×</a>
+                </span>
+            @endif
+            
+            @if(request('city'))
+                <span class="inline-flex items-center px-3 py-1 rounded-full text-sm bg-pink-100 text-pink-800">
+                    Kota: {{ request('city') }}
+                    <a href="{{ route('events.index', array_diff_key(request()->all(), ['city' => ''])) }}" class="ml-2 text-pink-600 hover:text-pink-800">×</a>
+                </span>
+            @endif
+        </div>
+    @endif
+
+    <!-- Results Count -->
+    <div class="mb-4 text-gray-600">
+        Menampilkan <strong>{{ $events->total() }}</strong> event
     </div>
 
     <!-- Events Grid -->
     <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         @forelse($events as $event)
-            <div class="bg-white rounded-lg shadow-md overflow-hidden">
+            <div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 border border-gray-100">
                 @if($event->poster)
-                    <img src="{{ Storage::url($event->poster) }}" alt="{{ $event->title }}" class="w-full h-48 object-cover">
+                    <div class="relative">
+                        <img src="{{ Storage::url($event->poster) }}" alt="{{ $event->title }}" class="w-full h-48 object-cover">
+                        <div class="absolute top-3 left-3">
+                            <span class="bg-white/95 backdrop-blur-sm text-blue-600 px-3 py-1 rounded-full text-xs font-semibold shadow-lg">
+                                {{ $event->sport->icon }} {{ $event->sport->name }}
+                            </span>
+                        </div>
+                    </div>
                 @else
-                    <div class="w-full h-48 bg-gradient-to-r from-blue-400 to-purple-400 flex items-center justify-center">
-                        <span class="text-6xl">{{ $event->sport->icon }}</span>
+                    <div class="relative w-full h-48 bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+                        <span class="text-7xl opacity-70">{{ $event->sport->icon }}</span>
+                        <div class="absolute top-3 left-3">
+                            <span class="bg-white/95 backdrop-blur-sm text-blue-600 px-3 py-1 rounded-full text-xs font-semibold shadow-lg">
+                                {{ $event->sport->name }}
+                            </span>
+                        </div>
                     </div>
                 @endif
-                <div class="p-4">
-                    <span class="text-sm text-blue-600 font-semibold">{{ $event->sport->name }}</span>
-                    <h3 class="text-lg font-bold mt-1">{{ $event->title }}</h3>
-                    <p class="text-gray-600 text-sm mt-2">
-                        📅 {{ $event->tanggal_mulai->format('d M Y, H:i') }}<br>
-                        📍 {{ $event->lokasi }}
-                    </p>
+                
+                <div class="p-5">
+                    <h3 class="text-lg font-bold text-gray-900 mb-3 line-clamp-2 hover:text-blue-600 transition-colors">
+                        {{ $event->title }}
+                    </h3>
+                    
+                    <div class="space-y-2 text-sm text-gray-600 mb-4">
+                        <div class="flex items-center">
+                            <span class="w-5 flex-shrink-0">📅</span>
+                            <span class="ml-1">{{ $event->tanggal_mulai->format('d M Y, H:i') }} WIB</span>
+                        </div>
+                        <div class="flex items-start">
+                            <span class="w-5 flex-shrink-0 mt-0.5">📍</span>
+                            <span class="ml-1 line-clamp-2">{{ $event->lokasi }}</span>
+                        </div>
+                    </div>
+                    
                     @if($event->tickets->count() > 0)
-                        <p class="text-green-600 font-semibold mt-2">
-                            Mulai Rp {{ number_format($event->tickets->min('harga'), 0, ',', '.') }}
-                        </p>
+                        <div class="flex items-center justify-between mb-4 pb-4 border-b border-gray-100">
+                            <div>
+                                <span class="text-xs text-gray-500 block">Mulai dari</span>
+                                <span class="text-lg font-bold text-emerald-600">
+                                    Rp {{ number_format($event->tickets->min('harga'), 0, ',', '.') }}
+                                </span>
+                            </div>
+                            <div class="text-right">
+                                <span class="text-xs text-gray-500 block">Tersedia</span>
+                                <span class="text-sm font-semibold text-gray-700">
+                                    {{ $event->tickets->sum('kuota') }} tiket
+                                </span>
+                            </div>
+                        </div>
                     @endif
+                    
                     <a href="{{ route('events.show', $event) }}" 
-                       class="mt-4 block text-center bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition">
-                        Lihat Detail
+                       class="block text-center bg-gradient-to-r from-blue-600 to-purple-600 text-white py-2.5 rounded-lg hover:from-blue-700 hover:to-purple-700 transition font-medium shadow-md hover:shadow-lg">
+                        Lihat Detail & Beli Tiket
                     </a>
                 </div>
             </div>
         @empty
-            <div class="col-span-full text-center py-12">
-                <p class="text-gray-500">Tidak ada event yang ditemukan.</p>
+            <div class="col-span-full text-center py-16">
+                <div class="text-6xl mb-4">🔍</div>
+                <p class="text-xl text-gray-700 font-medium mb-2">Tidak ada event yang ditemukan</p>
+                <p class="text-gray-500 mb-6">Coba ubah filter pencarian Anda</p>
+                <a href="{{ route('events.index') }}" class="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+                    Reset Filter
+                </a>
             </div>
         @endforelse
     </div>
 
     <!-- Pagination -->
-    <div class="mt-8">
-        {{ $events->links() }}
-    </div>
+    @if($events->hasPages())
+        <div class="mt-8">
+            {{ $events->appends(request()->query())->links() }}
+        </div>
+    @endif
 </div>
+
+<!-- JavaScript for Dynamic City Dropdown -->
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const provinceSelect = document.getElementById('provinceSelect');
+    const citySelect = document.getElementById('citySelect');
+    
+    // Data kota per provinsi
+    const citiesData = {!! json_encode($allCities ?? []) !!};
+    
+    provinceSelect.addEventListener('change', function() {
+        const selectedProvince = this.value;
+        
+        // Reset city dropdown
+        citySelect.innerHTML = '<option value="">Semua Kota/Kabupaten</option>';
+        
+        if (selectedProvince && citiesData[selectedProvince]) {
+            // Enable city dropdown
+            citySelect.disabled = false;
+            
+            // Populate cities
+            citiesData[selectedProvince].forEach(city => {
+                const option = document.createElement('option');
+                option.value = city;
+                option.textContent = city;
+                citySelect.appendChild(option);
+            });
+        } else {
+            citySelect.disabled = true;
+            citySelect.innerHTML = '<option value="">Pilih Provinsi Dulu</option>';
+        }
+    });
+    
+    // Trigger change event if province is already selected (for page reload)
+    if (provinceSelect.value) {
+        provinceSelect.dispatchEvent(new Event('change'));
+        
+        // Set selected city if exists
+        const selectedCity = '{{ request("city") }}';
+        if (selectedCity) {
+            setTimeout(() => {
+                citySelect.value = selectedCity;
+            }, 100);
+        }
+    }
+});
+</script>
 @endsection
